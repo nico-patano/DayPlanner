935968f7dd890026a9ce821da1b209f8b626ae4e
Nicholas Patano <nicholaspatano@gmail.com>
2022-01-26 20:08:30 -0500

    added styling and clickable event

--- script/script.js
+++ script/script.js
@@ -1,5 +1,23 @@
 // displays current date under main heading
 let today = moment();
-$("#currentDay").text(today.format("MMMM Do, YYYY"));
+ $("#currentDay").text(today.format("dddd, MMM do, YYYY"));
+//
 
-// 
+
+// save button
+
+let saveButton = document.getElementById("saveButton");
+let impValue = document.getElementById("impValue");
+
+saveButton.onclick = function () {
+
+  let value = impValue.value;
+  alert(value);
+ 
+
+};
+//
+
+
+
+
