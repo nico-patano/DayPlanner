4d900a2736f6c329a6bd27e1a3fdd9964e5bc3a3
Nicholas Patano <nicholaspatano@gmail.com>
2022-01-28 09:54:24 -0500

    added get item code to save tasks on reload

--- script/script.js
+++ script/script.js
@@ -8,18 +8,23 @@ $("#currentDay").text(today.format("dddd, MMM do, YYYY"));
 $(".time-block").each(function () {
   let saveButton = $(this).find('#saveButton');
   let impValue = $(this).find('#impValue');
+  let id = $(this).attr('id');
   
   $(saveButton).click(function () {
-    
-    localStorage.setItem('inputBox', impValue.val());
-    let saveInput = localStorage.getItem('inputBox')
-
+    localStorage.setItem(id, impValue.val());
+    let saveInput = localStorage.getItem(id)
+    console.log(saveInput);
   });
 });
 
 // code to push saved storage to text area
 
-
+$(".time-block").each(function () {
+  let impValue = $(this).find('#impValue');
+  let id = $(this).attr('id');
+  let saveValue = localStorage.getItem(id);
+  impValue.val(saveValue);
+});
 
 //area color change using jquery
 
